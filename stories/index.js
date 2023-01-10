f86c5c07fc569c3759b8a487421586484309506f
Brad Williams <bradley_williams2003@hotmail.com>
2023-01-10 10:48:55 -0400

    Add DayListItem component. Add to stories. Add classNames function to apply styles to DayListItems

--- stories/index.js
+++ stories/index.js
@@ -2,11 +2,16 @@ import React from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
+import DayListItem from "components/DayListItem";
+
+
 
 import "index.scss";
 
 import Button from "components/Button";
 
+
+// Button stories
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -21,4 +26,17 @@ storiesOf("Button", module)
     <Button disabled onClick={action("button-clicked")}>
       Disabled
     </Button>
+  ));
+
+
+//DayListItem stories
+storiesOf("DayListItem", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  }) // Provides the default background color for our component
+  .add("Unselected", () => <DayListItem name="Monday" spots={5} />)
+  .add("Selected", () => <DayListItem name="Monday" spots={5} selected />)
+  .add("Full", () => <DayListItem name="Monday" spots={0} />)
+  .add("Clickable", () => (
+    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} />
   ));
