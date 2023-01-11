752100a1aef0be6e54a1575c8ef9ebf95f997f2a
Brad Williams <bradley_williams2003@hotmail.com>
2023-01-11 14:41:33 -0400

    Add Empty component, Add Empty story to stories

--- stories/index.js
+++ stories/index.js
@@ -8,6 +8,8 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 import "index.scss";
 
@@ -152,4 +154,8 @@ storiesOf("Appointment", module)
     backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
   .add("Appointment", () => <Appointment />)
-  .add('Appointment with Time', () => <Appointment time={'12pm'} />);
+  .add('Appointment with Time', () => <Appointment time={'12pm'} />)
+  .add('Header', () => <Header time='12pm' />)
+  .add('Empty', () => <Empty onAdd={action('onAdd')} />)
+
+
